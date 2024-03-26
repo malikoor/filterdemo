4c28c4d4ca689e5e53e5c65f3278fa4ed1b7ffd2
malek <ramdani.malek@gmail.com>
2024-03-26 18:32:58 -0400

    Firs commit

---
+++ src/main/java/org/example/filterdemo/MyCustomFilter1.java
@@ -0,0 +1,27 @@
+package org.example.filterdemo;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyCustomFilter1 extends GenericFilter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // No initialization needed for this simple filter
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        httpResponse.addHeader("Filter-Processing-1", "CustomFilter1 Processing");
+        chain.doFilter(request, response); // Pass the request down the filter chain
+    }
+
+
+    @Override
+    public void destroy() {
+        // No cleanup needed for this simple filter
+    }
+}
---
+++ src/main/java/org/example/filterdemo/MyCustomFilter2.java
@@ -0,0 +1,28 @@
+package org.example.filterdemo;
+
+
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyCustomFilter2 extends GenericFilter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // No initialization needed for this simple filter
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        httpResponse.addHeader("Filter-Processing-2", "CustomFilter2 Processing");
+        chain.doFilter(request, response); // Pass the request down the filter chain
+    }
+
+    @Override
+    public void destroy() {
+        // No cleanup needed for this simple filter
+    }
+}
