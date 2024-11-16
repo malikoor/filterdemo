0ad6a6b5a2599e1a7e45213e9fa0dd9ac1e8ceec
malek <ramdani.malek@gmail.com>
2024-11-16 11:34:30 -0500

    Update url pattern in filter mapping for both filters

--- src/main/java/org/example/filterdemo/MyCustomFilter2.java
+++ src/main/java/org/example/filterdemo/MyCustomFilter2.java
@@ -3,6 +3,8 @@ package org.example.filterdemo;
 
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
